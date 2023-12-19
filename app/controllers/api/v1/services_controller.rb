f53657ddce9be25b546f919947da8101c05afc1a
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-19 13:45:21 -0300

    21° commit - tdd controller: Consegue listar um service especifico e retornar status 200? GREEN

--- app/controllers/api/v1/services_controller.rb
+++ app/controllers/api/v1/services_controller.rb
@@ -1,10 +1,14 @@
 class Api::V1::ServicesController < ApplicationController
 
-  #before_action :set_service, only: %i[] #show update destroy
+  before_action :set_service, only: %i[show] #show update destroy
 
   def index
     @services =Service.all 
     render json: @services
+  end
+
+  def show
+    render json: @service
   end
 
 private
