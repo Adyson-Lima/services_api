9c5f5529f2860049f3da6705826a215200093ea6
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-19 13:34:57 -0300

    18° commit - tdd controller: Consegue listar todos os services e retornar status 200? RED

---
+++ spec/controllers/services_controller_v1_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::ServicesController, type: :controller do
+
+  before{@service = Service.create(name: 'Backup', value: 'R$100,00')}
+
+  describe 'GET /api/v1/services' do
+    it 'Consegue listar todos os services e retornar status 200?' do
+      get :index
+      expect(JSON.parse(response.body).size).to eq(200)
+      expect(response).to have_http_status(200)
+    end
+  end
+
+end
