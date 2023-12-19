452eb91a5853cc67310eeb897cf9fcf7cabf837a
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-19 13:44:14 -0300

    20° commit - tdd controller: Consegue listar um service especifico e retornar status 200? RED

--- spec/controllers/services_controller_v1_spec.rb
+++ spec/controllers/services_controller_v1_spec.rb
@@ -12,4 +12,12 @@ RSpec.describe Api::V1::ServicesController, type: :controller do
     end
   end
 
+  describe 'GET /api/v1/services/id' do
+    it 'Consegue listar um service especifico e retornar status 200?' do
+      get :show, params: {id: @service.id}
+      expect(response.body).to include_json(name: 'Backup')
+      expect(response).to have_http_status(200)
+    end
+  end
+
 end
